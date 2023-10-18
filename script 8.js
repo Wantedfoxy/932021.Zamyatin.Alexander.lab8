ed1e834975b151f3a26c002c5ec67d6289993674
Alexander <sanjzamik10000@mail.ru>
2023-10-18 14:09:09 +0700

    Исправил ошибку, теперь сохраняются изменения при изменении порядка элементов

--- script 8.js
+++ script 8.js
@@ -22,7 +22,7 @@ function createNewElement() {
 
 function printResults() {
     const result = {};
-    document.querySelectorAll('.element').forEach((element, index) => {
+    document.querySelectorAll('.element').forEach((element) => {
         const label = element.querySelector('input[type="text"]').value;
         const number = element.querySelector('input[type="number"]').value;
         result[`${label}`] = `${number}`;
@@ -36,6 +36,7 @@ function upElement(element) {
     const previousElement = element.previousElementSibling;
     if (previousElement) {
         element.parentNode.insertBefore(element, previousElement);
+        updateElementsArray();
     }
 }
 
@@ -43,11 +44,21 @@ function downElement(element) {
     const nextElement = element.nextElementSibling;
     if (nextElement) {
         element.parentNode.insertBefore(nextElement, element);
+        updateElementsArray();
     }
 }
 
 function deleteElement(element) {
     element.parentNode.removeChild(element);
+}
+
+function updateElementsArray() {
+    elements = [];
+    document.querySelectorAll('.element').forEach(element => {
+        const label = element.querySelector('input[type="text"]').value;
+        const number = element.querySelector('input[type="number"]').value;
+        elements.push({ label, number });
+    })
 }
 
 document.addEventListener('DOMContentLoaded', createNewElement);
